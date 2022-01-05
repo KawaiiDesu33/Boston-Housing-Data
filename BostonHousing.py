c40159b5439a9359b7963575fb2823cb51cbd64a
KawaiiDesu33 <90171572+KawaiiDesu33@users.noreply.github.com>
2022-01-05 22:21:46 +1100

    Update BostonHousing.py

--- BostonHousing.py
+++ BostonHousing.py
@@ -10,6 +10,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn import preprocessing
 
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
@@ -53,13 +54,48 @@ sns.relplot(x = 'INDUS', y = 'RM', data = data, kind = 'scatter')
 ### Regressions
 
 # - All Variables
-reg_all_model = smf.ols(formula = 'MEDV ~ CRIM + ZN + INDUS + NOX + RM +\
+reg_all_model = smf.ols(formula = 'MEDV ~ CRIM + ZN + INDUS + + CHAS + NOX + RM +\
                         AGE + DIS + RAD + TAX + PTRATIO + B + LSTAT',
                         data = data)
 reg_results = reg_all_model.fit()
 print(reg_results.summary())
-
 ## Strong Fit, INDUS and AGE NOT Significant
+
+# - Assigning Regression Values
+reg_params = pd.DataFrame(reg_results.params)
+
+int_coef = reg_params.loc['Intercept', 0]
+CRIM_coef = reg_params.loc['CRIM', 0]
+ZN_coef = reg_params.loc['ZN', 0]
+INDUS_coef = reg_params.loc['INDUS', 0]
+CHAS_coef = reg_params.loc['CHAS', 0]
+NOX_coef = reg_params.loc['NOX', 0]
+RM_coef = reg_params.loc['RM', 0]
+AGE_coef = reg_params.loc['AGE', 0]
+DIS_coef = reg_params.loc['DIS', 0]
+RAD_coef = reg_params.loc['RAD', 0]
+TAX_coef = reg_params.loc['TAX', 0]
+PT_coef = reg_params.loc['PTRATIO', 0]
+B_coef = reg_params.loc['B', 0]
+LSTAT_coef = reg_params.loc['LSTAT', 0]
+    
+temp = []
+
+# - Predicted Y hats
+for i in range(0, 506, 1):
+    a = (int_coef + CRIM_coef*data.iloc[i, 0] + ZN_coef*data.iloc[i, 1] +
+        INDUS_coef*data.iloc[i, 2] + CHAS_coef*data.iloc[i, 3]
+        + NOX_coef*data.iloc[i, 4] + RM_coef*data.iloc[i, 5] + AGE_coef*data.iloc[i, 6]
+        + DIS_coef*data.iloc[i, 7] + RAD_coef*data.iloc[i, 8] + 
+        TAX_coef*data.iloc[i, 9] + PT_coef*data.iloc[i, 10] + 
+        B_coef*data.iloc[i, 11] + LSTAT_coef*data.iloc[i, 12])
+    temp.append(a)
+    
+yhat = pd.DataFrame(temp)
+yhat.columns = ['yhat']
+
+data_tot = data.join(yhat)
+
 
 # Plots of reg_results
 figure1 = sm.graphics.plot_regress_exog(reg_results, 'CRIM')
@@ -68,9 +104,20 @@ figure2 = sm.graphics.plot_regress_exog(reg_results, 'RM')
 
 figure_resid = sm.graphics.plot_leverage_resid2(reg_results)
 
+figure3 = sm.graphics.qqplot(data['MEDV'], fit = True, line = '45')
+plt.show()
+
+# Plotting Residuals
+resid = data_tot['MEDV'] - data_tot['yhat']
+norm_resid = preprocessing.normalize([resid])
+norm_resid = norm_resid.reshape(506, 1)
+
+x = range(0, 506, 1)
+
+plt.scatter(x=x, y = norm_resid)
+plt.ylabel('Normalised Residuals')
+plt.title('Normalised Residuals in Data')
+plt.show()
 
 
-
-
-
-
+figure4 = sm.graphics.qqplot(resid)
